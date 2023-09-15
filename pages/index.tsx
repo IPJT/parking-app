f8a0cfefde042aecddd25281685b3c06e793c595
Ian Thorslund <ian.thorslund@gmail.com>
2023-09-15 18:23:24 +0200

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import type { NextPage } from 'next'
 import { VehicleSelector } from '../components/VehicleSelector'
 import { Layout } from '../components/layout'
-import { useAuth } from '@clerk/nextjs'
 
 const Home = () => {
   return (

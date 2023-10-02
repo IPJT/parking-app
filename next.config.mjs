442ec658fac1b8e33da49194be4a7f0ee4713c6a
Ian Thorslund <ian.thorslund@gmail.com>
2023-10-02 21:53:14 +0200

    Adds sentry

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { withSentryConfig } from '@sentry/nextjs'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+  },
+}
+
+export default withSentryConfig(nextConfig)
